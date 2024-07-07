aef84eec13927d4e319bdf0c7379ec1a508ff797
Avinash Amballa <amballaavinash@gmail.com>
2024-07-07 16:29:55 -0400

    read me

--- inference.py
+++ inference.py
@@ -8,12 +8,3 @@ prompt = "A photo of sks dog in a bucket"
 image = pipe(prompt, num_inference_steps=50, guidance_scale=7.5).images[0]
 
 image.save("output_imgs/dog-bucket.png")
-
-
-
-
-# LORA inference
-from diffusers import DiffusionPipeline
-pipe = DiffusionPipeline.from_pretrained("base-model-name").to("cuda")
-pipe.load_lora_weights("path-to-the-lora-checkpoint")
-image = pipe("A picture of a sks dog in a bucket", num_inference_steps=25).images[0]
